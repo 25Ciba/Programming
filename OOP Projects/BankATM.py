2286cde00059629d0ac8fd69e53125d3bb6e7c7f
Derin <derinciba08@gmail.com>
2026-05-02 11:39:24 +0100

    fixed logic errors in Bank + cleaned up code

--- OOP Projects/BankATM.py
+++ OOP Projects/BankATM.py
@@ -1,29 +1,22 @@
 #-----------------------------------------------------------
 class Bank():
 
+    bankTotal = 100000  
+
     # Constructor
     def __init__(self, AccountName, Bal):
 
         # Attributes
-
         self.account = AccountName
         self.balance = Bal
-        self.bankTotal = 100000
 
 #-----------------------------------------------------------
 
     # Getters
 
-    # def getBankTotal(self):
-    #     return self.bankTotal
-
-    ###
-
     def getBal(self):
         print(f"Account holder: {self.account}")
         print(f"Current balance: ${self.balance}")
-
-    ###
 
     def getAccount(self):
         return self.account
@@ -39,31 +32,30 @@ class Bank():
             print("You do not posses enuf mula brody. Come back with more bags.")
         else:
             self.balance -= mula
+            Bank.bankTotal -= mula  
             print(f"Withdrew £{mula}. New balance: £{self.balance}")
             print(f"- Bank loses: £{mula}")
-            print(f"- Current Bank balance £{self.bankTotal - mula}")
-
-    ###
+            print(f"- Current Bank balance £{Bank.bankTotal}")  
 
     def deposit(self, mula):
 
         self.balance += mula
+        Bank.bankTotal += mula 
         print(f"Deposited £{mula}. New balance: £{self.balance}")
         print(f"- Bank loses: £{mula}")
-        print(f"- Current Bank balance £{self.bankTotal + mula}")
+        print(f"- Current Bank balance £{Bank.bankTotal}") 
 
 #-----------------------------------------------------------
+
+#Importing things
 
 import itertools
 import threading
 import time
 import sys
 
-
-
-
-
 #-----------------------------------------------------------
+
 # Accounts
 
 AccountS = [
@@ -78,16 +70,26 @@ AccountS = [
             Bank("Glaceon", 19005), 
             Bank("Proffessor Oak", 4113), 
             Bank("Ash Ketchum", 509)
-
             ]
 
 #-----------------------------------------------------------
 
 # Main program methods
 
-def getAccountS():
+def printAllAccountS():
     for i in range (len(AccountS)):
         print(AccountS[i].getAccount())
+
+
+def get_input(prompt):
+    return input(prompt).strip().lower()
+
+def findAccount(name):  
+    for account in AccountS:
+        if name == account.getAccount().lower():
+            return account
+    return None
+
 
 def animate():
     done = False
@@ -100,78 +102,77 @@ def animate():
             time.sleep(0.1)
     t = threading.Thread(target=animate)
     t.start()
-    time.sleep(2) #Change how long animation is
+    time.sleep(2)
     done = True
 
+
 def addAccountS():
-    inputName = input("Enter account: ")
+    inputName = input("Enter account: ").strip()  
     inputBal = int(input("Enter your current balance: "))
     newAccount = Bank(inputName, inputBal)
     AccountS.append(newAccount)
-    print("Registration successful! Thank you for using out services!")
+    print("Registration successful!")
+
 
 def CheckIn():
-    print("Welcome to the most prestigeous bank in the world (not really - im kidding dont sue me Goldman Sachs).")
-    print("Before any transactions please input the name of the account holder and current balance.")
-    accountName = input("Enter account: ")
-    accountBal = int(input("Enter your current balance: "))
-    checkAccount = Bank(accountName, accountBal)
-    for i in range (len(AccountS)):
-        if checkAccount in (AccountS[i]):
-            print(f"Welcome back {checkAccount.getAccount}!")
+    print("Welcome to the most prestigeous bank in the world.")
+    accountName = get_input("Enter account: ")
+    found = False
+    for account in AccountS:
+        if accountName == account.getAccount().lower():
+            print(f"Welcome back {account.getAccount()}!")
+            found = True
+            break
+    if not found:
+        print("You dont seem to be registered as an account holder.")
+        joinBank = get_input("Would you like to create a new account? (Y/N): ")
+
+        if joinBank == 'y':
+            addAccountS()
         else:
-            print(f"You dont seem to be registered as an account holder within this establishment.")
-            joinBank = input("Would you like to create a new account? Please anser with a prompt 'Y' or 'N' respectively: ")
-            if joinBank == 'Y':
-                addAccountS()
-            else:
-                print("Thank you for using our services. Have a great day, goodbye.")
+            print("Thank you for using our services. Goodbye.")
+            return  
 
 def CheckBal():
-    accName = input("Enter account holder name/ID: ")
-    for i in range (len(AccountS)):
-        if accName in (AccountS[i].getAccount()):
-            print(accName.getBal())
-
-
-
-#-----------------------------------------------------------
-
-#Choice Menu
+    accName = get_input("Enter account holder name/ID: ")
+    account = findAccount(accName) 
+    if account:
+        account.getBal()
+    else:
+        print("You are not registered with this bank")
 
 def menuChoice():
     CheckIn()
-    print('''
-    ==                                                                  ==
-    ==                           |Menu|                                 ==
-    ==                                                                  ==
-    == Please choose an option below by entering the number accordingly ==
-    ==                                                                  ==
-    ==                                                                  ==
-    ==                                                                  ==
-    ==  1. Create a new account                                         ==
-    ==  2. Deposit                                                      ==
-    ==  3. Withdraw                                                     ==
-    ==  4. Check Current Balance                                        ==
-    ==  5. Exit                                                         ==
-    ==                                                                  ==
-    ==                                                                  ==
-    ''')
 
-    # choice = input("Enter Number Here: ")
+    while True:
+        print("""
+1. Create a new account
+2. Deposit
+3. Withdraw
+4. Check Current Balance
+5. Exit
+""")
 
-    # if choice == 1:
-    #     addAccountS()
-    # elif choice ==2:
-    
-    # elif choice ==3:
+        choice = get_input("Enter Number Here: ")
 
-    # elif choice ==4:
+        if choice == '1':
+            addAccountS()
+        elif choice == '2':
+            print("Deposit not implemented yet")
+        elif choice == '3':
+            print("Withdraw not implemented yet")
+        elif choice == '4':
+            CheckBal()
+        elif choice == '5':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid option, try again.")
 
-    # elif choice ==5:
+#-----------------------------------------------------------
+                    ##############
+                    #MAIN PROGRAM#
+                    ##############
+#-----------------------------------------------------------
 
-#Figure out how to do num 4 in
-#Maybe do this same ything but instead of an arry use a linke list.
-#Then traverse linked list to find if person is in bank, use getters to find balance/name ect (element of the 'node')
-#There will be a problem when you add to the list though
-#lowkey good luck twin. Your gonna need it. (╥﹏╥)✌︎︎
+menuChoice()  
