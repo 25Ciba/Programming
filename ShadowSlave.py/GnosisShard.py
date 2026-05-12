0a049bdc58eed6e9aafc83d97b0908cf76ffa35d
Derin Ciba <25CibaD@CFBS.Domain.Local>
2026-05-12 13:02:14 +0100

    Shadow Slave OOP

---
+++ ShadowSlave.py/GnosisShard.py
@@ -0,0 +1,92 @@
+#Make a human and monster rankings
+
+#HUMAN
+
+#Rank
+#Aspects - active
+#Flaw - passive
+#Attributes - passive
+#Memories - array
+#Echoes
+#Hp - base
+#Atk - base
+#Defence - Base or +- memory
+#Soul core - mana
+#True name - array
+
+#======
+
+#MONSTER
+
+#Rank
+#Hp - base
+#Atk - base
+#Defence - Base
+
+#======
+
+
+# class Human:
+#     def __init__(self, name):
+#         self.name = name
+#         self.inventory = []
+
+#     def add_item(self, item):
+#         self.inventory.append(item)
+#         print(f"{self.name} picked up {item}")
+
+#     def remove_item(self, item):
+#         if item in self.inventory:
+#             self.inventory.remove(item)
+#             print(f"{self.name} dropped {item}")
+
+#     def show_inventory(self):
+#         print(f"{self.name}'s inventory: {self.inventory}")
+
+
+class Human():
+
+    #Constructor
+    def __init__(self, HName, TrueName, HRank, HClass, Aspects, Flaw, Attributes, HHP, HAtk, HDefence, HSC):
+        self._HName = HName
+        self._TrueName = TrueName
+        self._HRank = HRank
+        self._HClass = HClass
+        self._H = HName
+        self._HName = HName
+        self._HName = HName
+        self._HName = HName
+        self._HName = HName
+
+#Aspects and Flaw can be in a file that is rnd chosen
+#
+#Idk how to make memories, echoes, aspects, attributes, have both str and int values and how to relay their function when in battle
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
